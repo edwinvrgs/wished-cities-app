644b35758cd854ca089478fce0d9fcb8b5e017d9
Edwin Vargas <edwinleandrovargas@gmail.com>
2019-12-18 16:44:09 -0400

    Add backend connection and display of information received

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { APIClient }                  from './config/constants';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await APIClient.get('countries');
+        setData(response.data);
+      } catch (e) {
+        console.dir(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.map((country) => (
+          <p key={country}>{country}</p>
+        ))
+      }
     </div>
   );
-}
+};
 
 export default App;
