5dfea5ab79a6644c5dc05656e0c961386df47b79
Edwin Vargas <edwinleandrovargas@gmail.com>
2019-12-18 23:44:28 -0400

    Improve Dockerfile and connection with other containers

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,9 @@
 export const ENV = 'development';
 
+const { REACT_APP_BACK_PORT } = process.env;
+
 export const ENDPOINTS = {
-  development: 'http://localhost:3000/api',
+  development: `http://localhost:${REACT_APP_BACK_PORT}/api`,
   production: '',
   staging: '',
 };
